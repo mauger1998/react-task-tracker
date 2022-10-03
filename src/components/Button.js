0ff7b0c5aa0ebb8887a16ccfc04001cdfb3a8c3c
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-03 20:28:10 +0100

    Add onclick function to button with console.log

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types"
 
 const Button = ({color,text}) => {
+    const onClick = () => {
+        console.log("click");
+    }
   return (
     <div>
-      <button style={{backgroundColor: color}} className="btn">{text}</button>
+      <button onClick={onClick} style={{backgroundColor: color}} className="btn">{text}</button>
     </div>
   )
 }
