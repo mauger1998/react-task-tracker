0ba34dec198b6028040fa9238b0e9754088bf1ad
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-03 20:43:55 +0100

    Use map.task add h3 to tasks var in tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,13 +4,13 @@ const tasks = [
         text: "Doctors Appointment",
         day: "Feb 5th at 2:30pm",
         reminder: true;
-    }
+    },
     {
         id: 2,
         text: "Meeting at School",
         day: "Feb 6th at 1:30pm",
         reminder: true;
-    }
+    },
     {
         id: 3,
         text: "Food Shopping",
@@ -21,9 +21,11 @@ const tasks = [
 
 const Tasks = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    {tasks.map((task) => (
+    <h3>{task.text}</h3>
+    ))}
+    </>
   )
 }
 
