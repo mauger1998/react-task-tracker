6273daba98e7a5ce893232573b6d877d669a9938
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-03 21:11:10 +0100

    Add x icon in h3 via FaTimes

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,8 +1,12 @@
+import {FaTimes} from "react-icons/fa"
+
 const Task = ({task}) => {
   return (
     <div className="task">
-      <h3>{task.text}</h3>
-      <p>{task.day}</p>
+        <h3>
+          {task.text} <FaTimes /> 
+        </h3>
+        <p>{task.day}</p>
     </div>
   )
 }
